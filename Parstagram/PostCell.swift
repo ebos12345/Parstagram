daa0e28e50c863bb7ae500b65d89c4406b027e2a
Ricky Bernal <rickyb@rickys-mbp.attlocal.net>
2019-03-26 22:24:57 -0700

    Finished Unit 5 of the assignment no bonus objectives

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Ricky Bernal on 3/26/19.
+//  Copyright © 2019 Ricky Bernal. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
